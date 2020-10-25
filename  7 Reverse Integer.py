c1c84010f0b5d7951d57e0bddc87fef7afb58264
Zhong Chu <zhong.chu20@imperial.ac.uk>
2020-10-25 16:30:19 +0000

    Create  7 Reverse Integer.py
    leetcode 7 Reverse Integer

---
+++  7 Reverse Integer.py
@@ -0,0 +1,22 @@
+class Solution(object):
+	def reverse(self, x):
+		"""
+		:type x: int
+		:rtype: int
+		"""
+		num = 0
+		if x>0:
+			a=x
+		else:
+			a=-x
+		while(a!=0):
+			temp = a%10
+			num = num*10 + temp
+			a = int(a/10)
+			
+		if x>0 and num<2147483647:
+			return num
+		elif x<0 and num<2147483647:
+			return -num
+		else:
+			return 0
