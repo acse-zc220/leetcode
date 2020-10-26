bd1b5f94c14f550a8eb9caeff22b0ff840b8b03f
Zhong Chu <zhong.chu20@imperial.ac.uk>
2020-10-26 15:16:49 +0000

    Create 26 Remove Duplicates from sorted Array.py

---
+++ 26 Remove Duplicates from sorted Array.py
@@ -0,0 +1,16 @@
+class Solution(object):
+	def removeDuplicates(self, nums):
+		"""
+		:type nums: List[int]
+		:rtype: int
+		"""
+		if not nums:
+			return 0
+		
+		count = 0
+		for i in range(len(nums)):
+			if nums[count] != nums[i]:
+				count +=1
+				nums[count] = nums[i]
+				
+		return count +1
