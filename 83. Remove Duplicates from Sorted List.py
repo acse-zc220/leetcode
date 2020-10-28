bca2ac1574e5b2f390ba53f33d65083510baca38
Zhong Chu <zhong.chu20@imperial.ac.uk>
2020-10-28 14:59:50 +0000

    Create 83. Remove Duplicates from Sorted List.py

---
+++ 83. Remove Duplicates from Sorted List.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+	def deleteDuplicates(self, head):
+		"""
+		:type head: ListNode
+		:rtype: ListNode
+		"""
+		
+		current = head
+		while current:
+			runner = current.next
+			while runner and current.val == runner.val:
+				runner = runner.next
+			current.next = runner
+			
+			current =runner
+		
+
+		return head
