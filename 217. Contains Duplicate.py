49aeae0ac21f7609bc535b970f6ab67ecbe047b1
Zhong Chu <zhong.chu20@imperial.ac.uk>
2021-02-20 18:46:11 +0000

    Create 217. Contains Duplicate.py

---
+++ 217. Contains Duplicate.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+class Solution(object):
+	def containsDuplicate(self, nums):
+		
+		"""
+		:type nums: List[int]
+		:rtype: bool
+		"""
+		nums.sort()
+		for i in range(len(nums)-1):
+			if nums[i]==nums[i+1]:
+				return True
+			
+		return False
