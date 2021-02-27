b73c385c6b43ee0a9da9f1797e500f41ede705a4
Zhong Chu <zhong.chu20@imperial.ac.uk>
2021-02-27 20:14:17 +0000

    Create 219. Contains Duplicate II.py

---
+++ 219. Contains Duplicate II.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+class Solution(object):
+	def containsNearbyDuplicate(self, nums, k):
+		"""
+		:type nums: List[int]
+		:type k: int
+		:rtype: bool
+		"""
+		
+		
+		lookup = {}
+		for i, num in enumerate(nums):
+			if num not in lookup:
+				lookup[num] = i
+			else:
+				if i - lookup[num] <= k:
+					return True
+				lookup[num] = i
+		return False
